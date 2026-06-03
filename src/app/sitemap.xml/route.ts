848f6e7db7df85d5b8156880742301d7946f94d6
Andiikaaa <andikarizkifebrian@gmail.com>
2026-06-03 21:54:33 +0700

    fix sitemap

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,35 +1,18 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  const baseUrl =
-    process.env.NEXT_PUBLIC_SITE_URL ||
-    'https://aura-laundry.vercel.app';
-
-  const routes = [
-    '/',
-    '/services',
-    '/pricelist',
-    '/promo',
-    '/contact',
-  ];
-
-  const urls = routes
-    .map(
-      (path) => `
-  <url>
-    <loc>${baseUrl}${path}</loc>
-  </url>`
-    )
-    .join('');
+  const baseUrl = "https://aura-laundry.vercel.app";
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls}
+  <url>
+    <loc>${baseUrl}/</loc>
+  </url>
 </urlset>`;
 
   return new NextResponse(sitemap, {
     headers: {
-      'Content-Type': 'application/xml',
+      "Content-Type": "application/xml",
     },
   });
 }
